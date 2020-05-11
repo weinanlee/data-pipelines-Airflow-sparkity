6ba20cacac03e7cac284256671993c3aa604f0f9
Weinan Li <weianlee0322@gmail.com>
2020-05-11 21:31:52 +0200

    DAG pipeline finished.

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -6,23 +6,21 @@ from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
                                 LoadDimensionOperator, DataQualityOperator)
 from helpers import SqlQueries
 
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
 
 default_args = {
     'owner': 'weinanlee',
     'start_date': datetime(2019, 1, 12),
     'depends_on_past': False,
-    'retries': 0,
-    'retry_delay' : 300,
-    'email_on_retry' : False
+    'retries': 3,
+    'retry_delay': 300,
+    'email_on_retry': False
 }
 
 dag = DAG('udac_example_dag',
           default_args=default_args,
-          description='Load and transform data from S3 to Redshift with Airflow',
+          description='Load and transform data in Redshift with Airflow',
           schedule_interval='@hourly',
-          catchup = False
+          catchup=False
         )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
@@ -47,36 +45,80 @@ stage_songs_to_redshift = StageToRedshiftOperator(
 
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
-    dag=dag
+    dag=dag,
+    table = 'songplays',
+    select_sql_stmt = SqlQueries.songplay_table_insert
 )
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-    dag=dag
+    dag=dag,
+    table = 'users',
+    select_sql_stmt = SqlQueries.user_table_insert
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    dag=dag
+    dag=dag,
+    table = 'songs',
+    select_sql_stmt = SqlQueries.song_table_insert
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-    dag=dag
+    dag=dag,
+    table = 'artists',
+    select_sql_stmt = SqlQueries.artist_table_insert
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-    dag=dag
+    dag=dag,
+    table = 'time',
+    select_sql_stmt = SqlQueries.time_table_insert
 )
+
+
+sql_count = 'SELECT COUNT(*) FROM {}'
+has_rows_checker = lambda records: len(records) == 1 and len(records[0]) == 1 and records[0][0] > 0
+has_no_rows_checker = lambda records: len(records) == 1 and len(records[0]) == 1 and records[0][0] == 0
 
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
-    dag=dag
+    dag=dag,
+    sql_stmts=(
+        sql_count.format('songplays'), sql_count.format('users'),
+        sql_count.format('songs'), sql_count.format('artists'),
+        sql_count.format('time'), 'SELECT COUNT(*) FROM users WHERE first_name IS NULL'
+    ),
+    result_checkers=(
+        has_rows_checker, has_rows_checker,
+        has_rows_checker, has_rows_checker,
+        has_rows_checker, has_no_rows_checker
+    ),
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
 ## DAG's dependencies
+
 start_operator >> stage_events_to_redshift 
 start_operator >> stage_songs_to_redshift 
+
+
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+
+
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+
+
+load_song_dimension_table >> run_quality_checks
+load_user_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+
+run_quality_checks >> end_operator
