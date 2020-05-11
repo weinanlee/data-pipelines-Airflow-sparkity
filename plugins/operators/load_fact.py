89e7f53a345d5f1d1bf5c3244f4c30454abeff1b
Weinan Li <weianlee0322@gmail.com>
2020-05-11 21:27:43 +0200

    Load_fact modul finished.

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -8,15 +8,28 @@ class LoadFactOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 table = '',
+                 redshift_conn_id = 'redshift',
+                 select_sql_stmt = '',
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.select_sql_stmt = select_sql_stmt
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        
+        ## Postgre Hook
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info("Deleting previous data from {}...".format(self.table))
+        delete_sql_stmt = 'DELETE FROM {}'.format(self.table)
+        redshift_hook.run(delete_sql_stmt)
+
+        insert_sql_stmt = 'INSERT INTO {} ({})'.format(self.table, self.select_sql_stmt)
+
+        # Load table
+        self.log.info("Loading data into {}.".format(self.table))
+        redshift_hook.run(insert_sql_stmt)
+        self.log.info("Finished loading data into {}.".format(self.table))
